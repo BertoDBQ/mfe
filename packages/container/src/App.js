2e17e8484e843410875ce749a3522a84e3e038a2
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-18 13:28:59 -0600

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/marketingApp';
+
+export default () => {
+    return (
+        <div>
+            <h1>Hi there from container!</h1>
+            <hr></hr>
+            <MarketingApp />
+        </div>
+    )
+};
