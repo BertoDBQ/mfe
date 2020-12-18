2e204d1d4b5050d9a41a9ae6829eaef3dcfab8a2
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-18 15:39:17 -0600

    Message change just to kick off Container workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/marketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hi there from container!</h1>
+            <h1>Container Heading!</h1>
             <hr></hr>
             <MarketingApp />
         </div>
