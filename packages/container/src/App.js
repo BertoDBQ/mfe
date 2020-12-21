c757898ba77ba3bd5523080a311d79a2aa9fed44
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-21 12:06:17 -0600

    text change to container app to kick off build activity

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/marketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hello from Container Heading!!!!!</h1>
+            <h1>Hello from Container Heading!!!</h1>
             <hr></hr>
             <MarketingApp />
         </div>
