3a21f655e2232de685b245e7b7812794af114c06
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-21 16:36:38 -0600

    implemented multi-tier navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    // default history is in development mode only
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+    
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     )
+
+    return {
+        // location is parameter but destructuring out pathname
+        onParentNavigate( {pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#_marketing-dev-root');
     if (el) {
-        mount(el);
+        mount(el, { defaultHistory: createBrowserHistory() });
     }
 }
 
