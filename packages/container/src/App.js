8735fbe2c1939645274a7bac7d55afeef384df27
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-21 12:59:56 -0600

    corrected case on marketingApp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
+import MarketingApp from './components/marketingApp';
 
 export default () => {
     return (
