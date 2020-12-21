e58fe2d3b3460f105974f3c3fd58e26a6e7ec861
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-21 12:56:35 -0600

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import MarketingApp from './components/marketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return (
-        <div>
-            <h1>Hello from Container Heading!!!</h1>
-            <hr></hr>
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     )
 };
