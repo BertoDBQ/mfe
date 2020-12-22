2c00613e845731b8eb9ab940edaa49e0004a6011
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-21 21:08:11 -0600

    Added AuthApp and performance considerations

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import MarketingApp from './components/marketingApp';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+import Header from './components/Header';
+import Progress from './components/Progress';
+
+// lazy load the marketing and auth apps - only load when showing
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/marketingApp'));
+// import AuthApp from './components/AuthApp';
+// import MarketingApp from './components/marketingApp';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co"
@@ -14,7 +21,12 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
